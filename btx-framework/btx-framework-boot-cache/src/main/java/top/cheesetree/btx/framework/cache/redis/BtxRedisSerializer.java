9cfa55d6d4f7ddad946f116a200f008dc8681b20
fanjunjie <fanjunjie@smartplg.com>
2023-05-15 09:11:46 +0800

    修正cache oom漏洞

--- btx-framework/btx-framework-boot-cache/src/main/java/top/cheesetree/btx/framework/cache/redis/BtxRedisSerializer.java
+++ btx-framework/btx-framework-boot-cache/src/main/java/top/cheesetree/btx/framework/cache/redis/BtxRedisSerializer.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.redis.serializer.RedisSerializer;
 
+import java.util.UUID;
+
 /**
  * @author van
  * @date 2022/2/18 14:49
@@ -21,4 +23,30 @@ public class BtxRedisSerializer {
     private RedisSerializer<?> hashValueSerializer = valueSerializer;
 
     private RedisSerializer<String> stringSerializer = RedisSerializer.string();
+
+    private String serializerTag;
+
+    public BtxRedisSerializer(String serializerTag) {
+        this.serializerTag = serializerTag;
+    }
+
+    public BtxRedisSerializer() {
+        this(UUID.randomUUID().toString());
+    }
+
+    @Override
+    public int hashCode() {
+        return this.serializerTag.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        return this.serializerTag.equals(((BtxRedisSerializer) o).getSerializerTag());
+    }
 }
