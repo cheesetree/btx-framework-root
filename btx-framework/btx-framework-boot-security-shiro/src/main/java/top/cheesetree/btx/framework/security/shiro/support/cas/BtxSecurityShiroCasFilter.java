4506d381886468426a297b7bd64f7cebd7986d11
fanjunjie <fanjunjie@smartplg.com>
2023-05-18 17:03:52 +0800

    修复cas登录后未回跳登录前地址

--- btx-framework/btx-framework-boot-security-shiro/src/main/java/top/cheesetree/btx/framework/security/shiro/support/cas/BtxSecurityShiroCasFilter.java
+++ btx-framework/btx-framework-boot-security-shiro/src/main/java/top/cheesetree/btx/framework/security/shiro/support/cas/BtxSecurityShiroCasFilter.java
@@ -20,7 +20,6 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.beans.Encoder;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.URLEncoder;
@@ -115,6 +114,7 @@ public class BtxSecurityShiroCasFilter extends AuthenticatingFilter {
                                 SerializerFeature.WriteMapNullValue));
                     }
                 } else {
+                    this.saveRequest(request);
                     if (StringUtils.hasLength(req.getQueryString())) {
                         reqUrlStr.append("?").append(req.getQueryString());
                     }
