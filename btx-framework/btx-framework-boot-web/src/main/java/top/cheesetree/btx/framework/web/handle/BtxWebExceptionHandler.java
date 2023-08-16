98de51a570d4175defe93206a97071a1369a7ce4
fanjunjie <fanjunjie@smartplg.com>
2023-08-16 14:29:50 +0800

    web增加bindexception默认处理

--- btx-framework/btx-framework-boot-web/src/main/java/top/cheesetree/btx/framework/web/handle/BtxWebExceptionHandler.java
+++ btx-framework/btx-framework-boot-web/src/main/java/top/cheesetree/btx/framework/web/handle/BtxWebExceptionHandler.java
@@ -2,14 +2,20 @@ package top.cheesetree.btx.framework.web.handle;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import top.cheesetree.btx.framework.core.constants.BtxMessage;
 import top.cheesetree.btx.framework.core.exception.BtxException;
 import top.cheesetree.btx.framework.core.exception.BusinessException;
 import top.cheesetree.btx.framework.core.json.CommJSON;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -38,6 +44,33 @@ public class BtxWebExceptionHandler {
             }
 
             log.error("系统异常:[{},{}]{}", err.getErrcode(), err.getMessage(), ex);
+        } else {
+            ret = new CommJSON(BtxMessage.UNKOWN_ERROR);
+            String reqid = UUID.randomUUID().toString();
+            ret.setMsg(String.format("%s:%s", ret.getMsg(), reqid));
+            log.error("系统异常[{}]:{}", reqid, ex);
+        }
+
+        return ret;
+    }
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(value = BindException.class)
+    public CommJSON validationErrorHandler(Exception ex) {
+        CommJSON ret;
+
+        if (ex instanceof BindException) {
+            BindException err = (BindException) ex;
+            List<String> errmsg = new ArrayList<>();
+            err.getBindingResult().getFieldErrors().forEach(e -> {
+                errmsg.add(e.getDefaultMessage());
+            });
+
+            ret = new CommJSON(BtxMessage.VALIDATE_ERROR.getCode(), "", StringUtils.collectionToDelimitedString(errmsg, ";"
+            ), null);
+
+            log.error("系统异常:[{},{}]{}", "", err.getMessage(), ex);
         } else {
             ret = new CommJSON(BtxMessage.UNKOWN_ERROR);
             String reqid = UUID.randomUUID().toString();
